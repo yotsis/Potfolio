71478b480fbfce0775a33824a65a87c24e3df25e
yotsi <yotsis@yahoo.com>
2021-01-07 22:45:13 +0200

    Added structure

---
+++ js/data/skills.js
@@ -0,0 +1,38 @@
+let listOfSkills = [
+    'Flutter',
+    'Dart',
+    'JavaScript',
+    'Node js',
+    'Firebase',
+    'MongoDB',
+    'PostgreSql',
+    'CSS',
+    'React',
+    'Bootstrap',
+    'Java',
+    'Mocha',
+    'HandleBars',
+    'Travis CI',
+    'Jest',
+    'Redis',
+    'Docker'
+]
+
+function dataFoctory() {
+
+    const skills = () => listOfSkills;
+
+    return {
+        skills
+    }
+
+
+}
+
+
+
+
+
+
+
+
